f5e482cffd40296d1c05763c140a52e134f07ff7
chinhdtth2009021 <chinhdtth2009021@fpt.edu.vn>
2022-08-18 10:12:36 +0700

    eaxam

---
+++ src/main/java/com/example/examcsw/ExamCswApplication.java
@@ -0,0 +1,13 @@
+package com.example.examcsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamCswApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamCswApplication.class, args);
+    }
+
+}
